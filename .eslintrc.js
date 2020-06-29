326b44eb361d41da137122f0a7ec0ce6e81db31d
Xiaoyun <yangx232@gmail.com>
2020-06-28 21:31:31 -0400

    set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  extends: [
+      "airbnb",
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["import", "@typescript-eslint", "react-hooks"],
+  parserOptions: {
+      ecmaFeatures: {
+          jsx: true
+      },
+      sourceType: "module",
+      useJSXTextNode: true,
+      project: "./tsconfig.json"
+  },
+  rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-member-accessibility": "off",
+      "import/extensions": [1, "never", { ts: "never", "json": "always" }],
+      "react/jsx-indent": ["error", 2],
+      "react/jsx-indent-props": ["error", 2],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "indent": ["error", 2],
+      "react/jsx-props-no-spreading": "off",
+      "quotes": ["error", "double"],
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      "comma-dangle": ["error", "never"],
+      "react/jsx-filename-extension": ['warn', {	
+          extensions: ['ts', '.tsx'],	
+        }
+      ],
+      "react/sort-comp": [1, {
+          "order": [
+              "constructor",
+              "lifecycle",
+              "everything-else",
+              "render"
+          ]
+        }]
+        
+  },
+  overrides: [
+      {
+          files: ["**/*.ts", "**/*.tsx"],
+          rules: {
+              "no-unused-vars": ["off"],
+              "semi": 1,
+              "quote-props": ["error", "as-needed"]
+          }
+      }
+  ],
+  settings: {
+      "import/resolver": {
+          node: {
+              extensions: [".js", ".jsx", ".ts", ".tsx"]
+          },
+          "eslint-import-resolver-typescript": true
+      },
+      "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"]
+      },
+      react: {
+          version: "detect"
+      }
+  },
+  env: {
+      jest: true,
+      browser: true
+  }
+};
--- src/pages/real-estate-financial-calc.tsx
+++ src/pages/real-estate-financial-calc.tsx
@@ -1,50 +1,69 @@
-import React from "react"
-import { PageProps } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { Link } from "gatsby"
+import React from "react";
+import { PageProps, Link } from "gatsby";
 
 // Material UI
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import SEO from "../components/seo";
+import Layout from "../components/layout";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
-  },
+    minWidth: 650
+  }
 });
 
 function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
+  return {
+    name, calories, fat, carbs, protein
+  };
 }
+const createNewCol = ({
+  name, liquidAsset, asking, offer, downPaymentPct, closing, interestRate, maintenance
+}: {
+  name: string,
+  liquidAsset: number,
+  asking: number,
+  offer: number,
+  downPaymentPct: number,
+  closing: number,
+  interestRate: number,
+  maintenance: number
+}) => {
 
+};
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
 ];
+const AddNewColumn = () => {
+  const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(1)
+    }
+  }));
+  const classes = useStyles();
 
-const Column = () => {
-
-}
-
+  return (
+    <Fab size="small" color="secondary" aria-label="add" className={classes.margin}>
+      <AddIcon />
+    </Fab>
+  );
+};
 const CalcTable = () => {
   const classes = useStyles();
-  const AddNewColumn = () => {
-    return (
-      <Fab size="small" color="secondary" aria-label="add" className={classes.margin}>
-        <AddIcon />
-      </Fab>
-    );
-  }
+
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} size="small" aria-label="a dense table">
@@ -73,28 +92,21 @@ const CalcTable = () => {
       </Table>
     </TableContainer>
   );
-  
-}
+};
 
-const Calc = () => {
+const Calc = () => (
+  <div style={{ width: "90%" }}>
+    <CalcTable />
+  </div>
+);
 
-  return (
-    <div style={{width: "90%"}}>
-      <CalcTable/>
-    </div>
-  )
-
-}
-
-const BuyHomeCalcPage = (props: PageProps) => {
-  return (
-    <Layout>
+const BuyHomeCalcPage = (props: PageProps) => (
+  <Layout>
     <SEO title="Page two" />
     <h1>Real Estate Financial Calculator</h1>
     <Calc />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-  )
-}
+);
 
-export default BuyHomeCalcPage
+export default BuyHomeCalcPage;
